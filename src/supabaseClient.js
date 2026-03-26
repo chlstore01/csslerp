c6296ddd4a63762e31fd3ef1f683f2fd33ffcd3a
Engr M Arman <chl.store01@gmail.com>
2026-03-26 06:16:32 +0600

    added site summary dashboard

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
-
-// This tells the app: "Look at Vercel's settings for the keys"
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// Replace the strings below with your actual Supabase project details
+const supabaseUrl = 'https://hyuhbmmvivynrvgogyuk.supabase.co' 
+const supabaseKey = ' sb_publishable_93BGt9Ihf41qoRoSC-XLKg_JbnxDF5w '
+export const supabase = createClient(supabaseUrl, supabaseKey)

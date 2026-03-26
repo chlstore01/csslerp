f3ac992d7e47cec18dc1d60d576dfb0c93326972
Engr M Arman <chl.store01@gmail.com>
2026-03-26 06:25:21 +0600

    added site summary dashboard

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,4 @@
 // Replace the strings below with your actual Supabase project details
 const supabaseUrl = 'https://hyuhbmmvivynrvgogyuk.supabase.co' 
-const supabaseKey = ' sb_publishable_93BGt9Ihf41qoRoSC-XLKg_JbnxDF5w '
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5dWhibW12aXZ5bnJ2Z29neXVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNTg5NjMsImV4cCI6MjA4OTgzNDk2M30.D8wcp_bX-exDe9b8BrugxExfjcALVnR_tyUv4D_onTY'
 export const supabase = createClient(supabaseUrl, supabaseKey)
